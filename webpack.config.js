f34ed7a50f4e266650b25bf4ed3e7a724c1595e1
jamesee <james.ee.developer@gmail.com>
2021-09-20 23:01:18 +0800

    webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,10 @@
+
 const path = require('path');
 
 module.exports = {
-  plugins: [
-    new HtmlWebpackPlugin({
-      hash: true,
-      title: 'Webpack Example App',
-      header: 'Webpack Example Title',
-      metaDesc: 'Webpack Example Description',
-      template: './public/index.html',
-      filename: 'index.html',
-      inject: 'body'
-    })
-  ],
-  mode: 'development'
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
 };
